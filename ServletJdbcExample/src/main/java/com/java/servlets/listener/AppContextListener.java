e83aacec6f642ba0e3b78b0320fa592bda9516e4
b.yacenko <b.yacenko@bftcom.com>
2016-11-04 18:41:34 +0300

    04-11-2016_02 Logging to separate log files

--- ServletJdbcExample/src/main/java/com/java/servlets/listener/AppContextListener.java
+++ ServletJdbcExample/src/main/java/com/java/servlets/listener/AppContextListener.java
@@ -1,17 +1,21 @@
 package com.java.servlets.listener;
 
 import com.java.servlets.util.DbUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 public class AppContextListener implements ServletContextListener{
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(AppContextListener.class);
+
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		DbUtil.setDbproperties(sce.getServletContext().getInitParameter("dbproperties"));
 		DbUtil.getConnection();
-		System.out.println("------debug----- context initialized success");
+		LOGGER.info("------debug----- context initialized success");
 	}
 
 	@Override
